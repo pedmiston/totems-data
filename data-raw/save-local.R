87f036659b63925a591590e1a971321ecbaac95b
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-10-09 16:59:26 -0500

    Script for saving local copy of tables

--- data-raw/save-local.R
+++ data-raw/save-local.R
@@ -3,13 +3,13 @@ library(dplyr)
 devtools::load_all()
 con <- connect_db()
 
-dst_dir <- "data-raw/tables"
+dst_dir <- "data-raw/tables/"
 if (!dir.exists(dst_dir)) dir.create(dst_dir)
 
 save_local <- function(name) {
   tbl(con, name) %>%
     collect() %>%
-    write.csv(paste0(dst_dir, name, ".csv"), rownames = FALSE)
+    write.csv(paste0(dst_dir, name, ".csv"), row.names = FALSE)
 }
 
-sapply(c("Table_Group", "Table_Player", "Table_Workshop"), save_local)
+invisible(sapply(c("Table_Group", "Table_Player", "Table_Workshop"), save_local))
