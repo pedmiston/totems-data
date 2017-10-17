0cd078fd5ad1a4c04ca5c4aed2aca74a7e754219
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-10-17 13:09:16 -0500

    Testing labeling in another way

--- tests/testthat/test_label_experiment.R
+++ tests/testthat/test_label_experiment.R
@@ -22,3 +22,19 @@ test_that("labeling player conditions does not duplicate synchronic players", {
   result <- label_player_conditions(frame)
   expect_equal(nrow(result), 1)
 })
+
+test_that("labeling player conditions duplicates guesses correctly", {
+  guesses <- data_frame(
+    Guess = 1,
+    Result = 1,
+    Strategy = c("Synchronic", "Diachronic", "Isolated", "Isolated"),
+    PlayersPerSession = c(2, 1, 1, 1),
+    SessionDuration = c(25, 25, 50, 25),
+    Generation = c(1, 1, 1, 1),
+    PlayerIX = 1,
+    SessionIX = 1
+  )
+  results <- label_player_conditions(guesses)
+  expect_equal(nrow(results), nrow(guesses) + 2)
+  expect_equal(results$Exp, c("50LaborMinutes", "50LaborMinutes", "100LaborMinutes", "50LaborMinutes", "50LaborMinutes", "100LaborMinutes"))
+})
