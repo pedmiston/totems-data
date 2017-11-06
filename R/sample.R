c726e07eb6939e4319fd0d9cf062964e71350a39
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-11-06 12:11:09 -0600

    Fill in missing values when sampling'

--- R/sample.R
+++ R/sample.R
@@ -1,5 +1,7 @@
 
 #' Sample guesses at regular intervals in a session.
+#'
+#' Requires SessionID, SessionDuration, SessionTime
 sample_session <- function(session_guesses) {
   session_duration <- session_guesses$SessionDuration[[1]]
   sampled_times <- seq(1, session_duration, by = 1)
@@ -8,5 +10,22 @@ sample_session <- function(session_guesses) {
   valid_sample_times <- dplyr::filter(sampled_indices, indices != 0)
   sampled_guesses <- session_guesses[valid_sample_times$indices, ]
   sampled_guesses$SampledSessionTime <- valid_sample_times$sampled_times
+
+  missing_sample_times <- dplyr::filter(sampled_indices, indices == 0) %>%
+    select(SampledSessionTime = sampled_times)
+  if(nrow(missing_sample_times) > 0) {
+    default <- head(session_guesses, n = 1) %>%
+      mutate(
+        Guess = NA_character_,
+        Result = NA_integer_,
+        PrevSessionInventoryID = "1-2-3-4-5-6",
+        SessionInventorySize = 6,
+        PrevSessionGuessesHash = digest::digest(""),
+        NumSessionGuesses = 0
+      )
+    missing <- merge(missing_sample_times, default)
+    sampled_guesses <- bind_rows(missing, sampled_guesses)
+  }
+
   sampled_guesses
 }
--- tests/testthat/test_sample.R
+++ tests/testthat/test_sample.R
@@ -8,18 +8,32 @@ test_that("sample trials works", {
 
 test_that("sample session works", {
   session <- data_frame(
+    SessionID = "S1",
     SessionDuration = 5,
-    SessionTime = seq(1, 5, by = 0.5)
+    SessionTime = seq(1, 5, length.out = 100)
   )
   result <- sample_session(session)
   expect_true("SampledSessionTime" %in% colnames(result))
+  expect_equal(nrow(result), 5)
 })
 
 test_that("sample session handles missing trials", {
   session <- data_frame(
+    SessionID = "S1",
     SessionDuration = 5,
-    SessionTime = seq(2, 5, by = 0.5)
+    SessionTime = seq(2, 5, length.out = 102)
   )
   result <- sample_session(session)
-  expect_equal(nrow(result), 4)
+  expect_equal(nrow(result), 5)
+})
+
+test_that("sample session retains additional columns", {
+  session <- data_frame(
+    SessionID = "S1",
+    SessionDuration = 5,
+    SessionTime = seq(1, 5, length.out = 102),
+    MyExtraVar = "a"
+  )
+  result <- sample_session(session)
+  expect_equal(session$MyExtraVar[[1]], "a")
 })
