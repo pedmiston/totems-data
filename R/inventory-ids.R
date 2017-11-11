8d1a6b79c7d1ffa603e5a0d88d964617cf0d0f51
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-11-11 15:24:02 -0600

    Record final inventory id in player performance

--- R/inventory-ids.R
+++ R/inventory-ids.R
@@ -4,7 +4,7 @@ calculate_final_inventory_id <- function(prev_inventory_id, result) {
   inventory_items <- strsplit(prev_inventory_id, split = "-") %>%
     purrr::map(as.integer) %>%
     unlist()
-  if(result %in% inventory_items) return(prev_inventory_id)
+  if(result %in% inventory_items | result == 0) return(prev_inventory_id)
   append(inventory_items, result) %>%
     sort() %>%
     paste(collapse = "-")
--- tests/testthat/test_inventory_ids.R
+++ tests/testthat/test_inventory_ids.R
@@ -9,3 +9,17 @@ test_that("duplicate results are ignored", {
   result <- calculate_final_inventory_id("1", 1)
   expect_equal(result, "1")
 })
+
+test_that("incorrect results are ignored", {
+  result <- calculate_final_inventory_id("1", 0)
+  expect_equal(result, "1")
+})
+
+context("Verify inventory ids")
+
+data("PlayerPerformance")
+
+test_that("0 doesn't appear in inventory ids", {
+  inventory_ids <- PlayerPerformance$FinalInventoryID
+  expect_true(!any(grepl("^0-", inventory_ids)))
+})
