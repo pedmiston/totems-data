ad695d97a6b612ece0b8653d6fd0c38213510f10
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-12-03 15:19:20 -0600

    Prevent get lm mods from dropping

--- R/model-utils.R
+++ R/model-utils.R
@@ -3,6 +3,6 @@
 get_lm_mod_preds <- function(lm_mod) {
   mod_data <- lm_mod$model
   params <- attr(lm_mod$terms, "term.labels")
-  x_preds <- unique(mod_data[, params])
+  x_preds <- unique(mod_data[, params, drop = FALSE])
   cbind(x_preds, predict(lm_mod, x_preds, se = TRUE))
 }
